2b3065fcf67be2cb74a1ee3783be2eb35b3b860a
Nebil Muhidin <nebilmuhidin57@gmail.com>
2024-07-16 23:02:36 +0300

    refactor the code

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker 
-from models.post import Post
-from models.user import User
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 import os
+
+
+Base = declarative_base()
+
 
 load_dotenv()
 
@@ -12,14 +15,9 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
-
-User.metadata.create_all(bind=engine)
-Post.metadata.create_all(bind=engine)
--- routes/post.py
+++ routes/post.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from schemas import post as schemas
+from schemas import post as post_schemas, user as user_schemas
 from services import post_service
 from dependencies.auth import get_current_user
 from database import get_db
@@ -8,13 +8,13 @@ from utils.cache import get_cached_posts, set_cached_posts
 
 router = APIRouter()
 
-@router.post("/posts", response_model=schemas.PostInDB)
-def add_post(post: schemas.PostCreate, current_user: schemas.UserInDB = Depends(get_current_user), db: Session = Depends(get_db)):
+@router.post("/posts", response_model=post_schemas.PostInDB)
+def add_post(post: post_schemas.PostCreate, current_user: user_schemas.UserInDB = Depends(get_current_user), db: Session = Depends(get_db)):
     db_post = post_service.create_post(db, post, current_user.id)
     return db_post
 
-@router.get("/posts", response_model=list[schemas.PostInDB])
-def get_posts(current_user: schemas.UserInDB = Depends(get_current_user), db: Session = Depends(get_db)):
+@router.get("/posts", response_model=list[post_schemas.PostInDB])
+def get_posts(current_user: user_schemas.UserInDB = Depends(get_current_user), db: Session = Depends(get_db)):
     cached_posts = get_cached_posts(current_user.id)
     if cached_posts:
         return cached_posts
@@ -22,8 +22,8 @@ def get_posts(current_user: schemas.UserInDB = Depends(get_current_user), db: Se
     set_cached_posts(current_user.id, db_posts)
     return db_posts
 
-@router.delete("/posts/{post_id}", response_model=schemas.PostInDB)
-def delete_post(post_id: int, current_user: schemas.UserInDB = Depends(get_current_user), db: Session = Depends(get_db)):
+@router.delete("/posts/{post_id}", response_model=post_schemas.PostInDB)
+def delete_post(post_id: int, current_user: user_schemas.UserInDB = Depends(get_current_user), db: Session = Depends(get_db)):
     db_post = post_service.delete_post(db, post_id, current_user.id)
     if db_post is None:
         raise HTTPException(status_code=404, detail="Post not found")
